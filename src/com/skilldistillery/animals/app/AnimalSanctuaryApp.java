e5b2a7b17c4bf7565dd389095003426f9b6ee003
Vonzink <vonzink@gmail.com>
2025-03-14 16:05:45 -0600

    inital project setup

---
+++ src/com/skilldistillery/animals/app/AnimalSanctuaryApp.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.animals.app;
+
+import java.util.Scanner; 
+
+public class AnimalSanctuaryApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Welcome to the Sanctuary Database "); 
+		System.out.println(" Welcome to the Sanctuary Database ");
+		
+		String input = sc.nextLine(); 
+
+
+		sc.close();
+	}
+
+}
---
+++ src/com/skilldistillery/animals/entities/Animal.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.animals.entities;
+
+public class Animal {
+
+}
